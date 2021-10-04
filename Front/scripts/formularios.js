c404b1ab5712c61f8839410cd72c05352d49f19c
AnahiMir20 <animiranda2000@hotmail.com>
2021-10-04 12:57:51 -0500

    Formulario flujos CD

--- Front/scripts/formularios.js
+++ Front/scripts/formularios.js
@@ -1,6 +1,44 @@
-function flujoAgregar(){
-    var flujoMes = document.getElementById("flujoMes").selectedIndex
+var flujos = {}
+var flujoMesAnterior = 0
+
+
+function flujoAgregar() {
+    if ($.isEmptyObject(flujos)) {
+        var flujoMes = document.getElementById("flujoMes").selectedIndex + 1
+        flujoMesAnterior = flujoMes
+    } else {
+        if (flujoMesAnterior == 12) {
+            flujoMes = 1
+            flujoMesAnterior = 1
+        } else {
+            flujoMes = flujoMesAnterior + 1
+            flujoMesAnterior += 1
+        }
+    }
     var flujoIngreso = document.getElementById('flujoIngreso').value;
-    console.log(flujoMes)
-    console.log(flujoIngreso)
+
+    nuevoFlujo = { mes: flujoMes, ingreso: Number(flujoIngreso) }
+    console.log(nuevoFlujo)
+    flujos[flujoMes] = nuevoFlujo
+    console.log(flujos);
+
+
+    $('#popup-flujo').fadeOut('slow');
+    $('.popup-overlay').fadeOut('slow');
+
+}
+
+function flujoEliminar() {
+    if ($.isEmptyObject(flujos)) {
+        alert('No hay nada para eliminar')
+    } else {
+        ultimoFlujo = 0
+       for(element  in flujos) {
+           
+           ultimoFlujo = element
+       }
+       console.log(ultimoFlujo)
+       delete flujos[ultimoFlujo];
+       flujoMesAnterior = ultimoFlujo - 1;
+    }
 }
--- Front/scripts/jquery.js
+++ Front/scripts/jquery.js
@@ -1,2 +1,39 @@
 $(function () {
-})
+    $('#crear-flujo').on('click', function () {
+        if ($.isEmptyObject(flujos)) {
+            $('#flujoMes').show()
+        } else {
+            $('#flujoMes').hide()
+            $('#listaTituloFlujo').hide()
+        }
+
+        $('#popup-flujo').fadeIn('slow');
+        $('.popup-overlay').fadeIn('slow');
+        $('.popup-overlay').height($(window).height());
+        return false;
+    });
+    $('#close-flujo').on('click', function () {
+        $('#popup-flujo').fadeOut('slow');
+        $('.popup-overlay').fadeOut('slow');
+        return false;
+    });
+
+    $('#eliminar-flujo').on('click', function () {
+        if ($.isEmptyObject(flujos)) {
+            alert('No hay nada a eliminar')
+        } else {
+            $('#popup-eliminar-flujo').fadeIn('slow');
+            $('.popup-overlay-eliminar').fadeIn('slow');
+            $('.popup-overlay-eliminar').height($(window).height());
+            return false;
+        }
+
+
+    });
+    $('#close-flujo-eliminar').on('click', function () {
+        $('#popup-eliminar-flujo').fadeOut('slow');
+        $('.popup-overlay-eliminar').fadeOut('slow');
+        return false;
+    });
+
+});
