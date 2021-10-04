a75a3e7e5fd033b122669c9e8a6642cc1dd1c015
AnahiMir20 <animiranda2000@hotmail.com>
2021-10-03 23:24:17 -0500

    Select check

--- Back/mvc/controller/presupuestosController.js
+++ Back/mvc/controller/presupuestosController.js
@@ -2,15 +2,51 @@ const presupuestosModel = require('../model/presupuestosModel')
 
 
 
+module.exports.select = async () => {
+    let response = new presupuestosModel();
+    let result = await response.select()
+    return result;
+}
+module.exports.find = async (presupuesto) => {
+    let response = new presupuestosModel();
+    let result = await response.find(presupuesto)
+    return result;
+}
 module.exports.insert = async (presupuesto) => {
     let response = new presupuestosModel();
     let result = await response.insert(presupuesto)
     return result;
 }
+module.exports.update = async (presupuesto) => {
+    let response = new presupuestosModel();
+    let result = await response.update(presupuesto)
+    return result;
+}
+module.exports.delete = async (presupuesto) => {
+    let response = new presupuestosModel();
+    let result = await response.delete(presupuesto)
+    return result;
+}
 
+module.exports.selectFlujo = async (flujo) => {
+    let response = new presupuestosModel();
+    let result = await response.selectFlujo(flujo)
+    return result;
+}
 module.exports.insertFlujo = async (flujo) => {
     let response = new presupuestosModel();
     let result = await response.insertFlujo(flujo)
+    return result;
+}
+module.exports.deleteFlujo = async (flujo) => {
+    let response = new presupuestosModel();
+    let result = await response.deleteFlujo(flujo)
+    return result;
+}
+
+module.exports.selectIngreso = async (ingreso) => {
+    let response = new presupuestosModel();
+    let result = await response.selectIngreso(ingreso)
     return result;
 }
 module.exports.insertIngreso = async (ingreso) => {
@@ -18,10 +54,31 @@ module.exports.insertIngreso = async (ingreso) => {
     let result = await response.insertIngreso(ingreso)
     return result;
 }
+module.exports.deleteIngreso = async (ingreso) => {
+    let response = new presupuestosModel();
+    let result = await response.deleteIngreso(ingreso)
+    return result;
+}
 
+module.exports.selectCosto = async (costo) => {
+    let response = new presupuestosModel();
+    let result = await response.selectCosto(costo)
+    return result;
+}
 module.exports.insertCosto = async (costo) => {
     let response = new presupuestosModel();
     let result = await response.insertCosto(costo)
+    return result;
+}
+module.exports.deleteCosto = async (costo) => {
+    let response = new presupuestosModel();
+    let result = await response.deleteCosto(costo)
+    return result;
+}
+
+module.exports.selectGasto = async (gasto) => {
+    let response = new presupuestosModel();
+    let result = await response.selectGasto(gasto)
     return result;
 }
 module.exports.insertGasto = async (gasto) => {
@@ -29,21 +86,24 @@ module.exports.insertGasto = async (gasto) => {
     let result = await response.insertGasto(gasto)
     return result;
 }
+module.exports.deleteGasto = async (gasto) => {
+    let response = new presupuestosModel();
+    let result = await response.deleteGasto(gasto)
+    return result;
+}
+
+module.exports.selectRecurso = async (recurso) => {
+    let response = new presupuestosModel();
+    let result = await response.selectRecurso(recurso)
+    return result;
+}
 module.exports.insertRecurso = async (recurso) => {
     let response = new presupuestosModel();
     let result = await response.insertRecurso(recurso)
     return result;
 }
-
-module.exports.updateProduct = async (product) => {
-    let response = new productsModel();
-    let result = await response.update(product)
-    return result;
-}
-
-
-module.exports.deleteProduct = async (productName) => {
-    let response = new productsModel();
-    let result = await response.delete(productName)
+module.exports.deleteRecurso = async (recurso) => {
+    let response = new presupuestosModel();
+    let result = await response.deleteRecurso(recurso)
     return result;
 }
--- Back/mvc/model/presupuestosModel.js
+++ Back/mvc/model/presupuestosModel.js
@@ -5,16 +5,49 @@ module.exports = class presupuestosModel {
         this.presupuesto = presupuesto
     }
 
+    async select() {
+        let result = await sequelize.query("SELECT * FROM presupuestos  ");
+        return result
+    }
+    async find(presupuesto) {
+        let result = await sequelize.query("SELECT * FROM presupuestos WHERE idPresupuesto = "+presupuesto.idPresupuesto);
+        return result
+    }
     async insert(presupuesto) {
         let result = await sequelize.query("INSERT INTO presupuestos (creacion,proyecto,[version]) VALUES ('" + presupuesto.date + "','" + presupuesto.proyecto + "', " + presupuesto.version + ")");
         console.log(result);
         if (result[0].length == 0) {
-            let result = await sequelize.query("SELECT idPresupuesto,creacion,proyecto,[version] FROM presupuestos  WHERE creacion ='" + presupuesto.date + "' AND proyecto ='" + presupuesto.proyecto + "' AND [version] = " + presupuesto.version );
+            let result = await sequelize.query("SELECT idPresupuesto,creacion,proyecto,[version] FROM presupuestos  WHERE creacion ='" + presupuesto.date + "' AND proyecto ='" + presupuesto.proyecto + "' AND [version] = " + presupuesto.version);
             return result
         }
         else {
             return false
         }
+    }
+    async update(presupuesto) {
+        let result = await sequelize.query("UPDATE presupuestos SET [version] =" + presupuesto.version + " WHERE idPresupuesto =" + presupuesto.idPresupuesto);
+        console.log(result);
+        if (result[0].length == 0) {
+            return result
+        }
+        else {
+            return false
+        }
+    }
+    async update(presupuesto) {
+        let result = await sequelize.query("DELETE FROM presupuestos WHERE idPresupuesto =" + presupuesto.idPresupuesto); console.log(result);
+        console.log(result);
+        if (result[0].length == 0) {
+            return result
+        }
+        else {
+            return false
+        }
+    }
+
+    async selectFlujo(flujo) {
+        let result = await sequelize.query("SELECT * FROM flujoEfectivo WHERE idFlujo = "+flujo.idPresupuesto);
+        return result
     }
     async insertFlujo(flujo) {
         let result = await sequelize.query("INSERT INTO flujoEfectivo (idPresupuesto,mes,ingreso) VALUES (" + flujo.idPresupuesto + "," + flujo.mes + ", " + flujo.ingreso + ")");
@@ -26,8 +59,8 @@ module.exports = class presupuestosModel {
             return false
         }
     }
-    async insertIngreso(ingreso) {
-        let result = await sequelize.query("INSERT INTO ingresos (idPresupuesto,concepto,mes,ingreso) VALUES (" + ingreso.idPresupuesto + ",'" + ingreso.concepto + "', " + ingreso.mes + "," +ingreso.ingreso +")");
+    async deleteFlujo(flujo) {
+        let result = await sequelize.query("DELETE FROM flujoEfectivo WHERE idFlujo =" + flujo.idFlujo); console.log(result);
         console.log(result);
         if (result[0].length == 0) {
             return result
@@ -37,8 +70,13 @@ module.exports = class presupuestosModel {
         }
     }
 
-    async insertCosto(costo) {
-        let result = await sequelize.query("INSERT INTO costosDirectos (idPresupuesto,concepto,mes,costo) VALUES (" + costo.idPresupuesto + ",'" + costo.concepto + "', " + costo.mes + "," +costo.costo +")");
+
+    async selectIngreso(ingreso) {
+        let result = await sequelize.query("SELECT * FROM ingresos WHERE idIngreso = "+ingreso.idPresupuesto);
+        return result
+    }
+    async insertIngreso(ingreso) {
+        let result = await sequelize.query("INSERT INTO ingresos (idPresupuesto,concepto,mes,ingreso) VALUES (" + ingreso.idPresupuesto + ",'" + ingreso.concepto + "', " + ingreso.mes + "," + ingreso.ingreso + ")");
         console.log(result);
         if (result[0].length == 0) {
             return result
@@ -46,9 +84,49 @@ module.exports = class presupuestosModel {
         else {
             return false
         }
+    }
+    async deletIngreso(ingreso) {
+        let result = await sequelize.query("DELETE FROM ingresos WHERE idIngreso =" + ingreso.idIngreso); console.log(result);
+        console.log(result);
+        if (result[0].length == 0) {
+            return result
+        }
+        else {
+            return false
+        }
+    }
+
+    async selectCosto(costo) {
+        let result = await sequelize.query("SELECT * FROM costosDirectos WHERE idCosto = "+costo.idPresupuesto);
+        return result
+    }
+    async insertCosto(costo) {
+        let result = await sequelize.query("INSERT INTO costosDirectos (idPresupuesto,concepto,mes,costo) VALUES (" + costo.idPresupuesto + ",'" + costo.concepto + "', " + costo.mes + "," + costo.costo + ")");
+        console.log(result);
+        if (result[0].length == 0) {
+            return result
+        }
+        else {
+            return false
+        }
+    }
+    async deleteCosto(costo) {
+        let result = await sequelize.query("DELETE FROM costosDirectos WHERE idCosto =" + costo.idCosto); console.log(result);
+        console.log(result);
+        if (result[0].length == 0) {
+            return result
+        }
+        else {
+            return false
+        }
+    }
+
+    async selectGasto(gasto) {
+        let result = await sequelize.query("SELECT * FROM gastosAdministrativos WHERE idGasto = "+gasto.idPresupuesto);
+        return result
     }
     async insertGasto(gasto) {
-        let result = await sequelize.query("INSERT INTO gastosAdministrativos (idPresupuesto,concepto,mes,gasto) VALUES (" + gasto.idPresupuesto + ",'" + gasto.concepto + "', " + gasto.mes + "," +gasto.gasto +")");
+        let result = await sequelize.query("INSERT INTO gastosAdministrativos (idPresupuesto,concepto,mes,gasto) VALUES (" + gasto.idPresupuesto + ",'" + gasto.concepto + "', " + gasto.mes + "," + gasto.gasto + ")");
         console.log(result);
         if (result[0].length == 0) {
             return result
@@ -57,9 +135,35 @@ module.exports = class presupuestosModel {
             return false
         }
     }
+    async deleteGasto(gasto) {
+        let result = await sequelize.query("DELETE FROM gastosAdministrativos WHERE idGasto =" + gasto.idGasto); console.log(result);
+        if (result[0].length == 0) {
+            console.log(result);
+            if (result[0].length == 0) {
+                return result
+            }
+            else {
+                return false
+            }
+        }
+    }
+
+    async selectRecurso(recurso) {
+        let result = await sequelize.query("SELECT * FROM recursos WHERE idRecurso = "+recurso.idPresupuesto);
+        return result
+    }
     async insertRecurso(recurso) {
-        let result = await sequelize.query("INSERT INTO recursos (idPresupuesto,rol,recurso,porcentaje,mes) VALUES (" + recurso.idPresupuesto + "," + recurso.rol + ", " + recurso.recurso + "," +recurso.porcentaje + "," +recurso.mes +")");
+        let result = await sequelize.query("INSERT INTO recursos (idPresupuesto,rol,recurso,porcentaje,mes) VALUES (" + recurso.idPresupuesto + "," + recurso.rol + ", " + recurso.recurso + "," + recurso.porcentaje + "," + recurso.mes + ")");
         console.log(result);
+        if (result[0].length == 0) {
+            return result
+        }
+        else {
+            return false
+        }
+    }
+    async deleteRecurso(recurso) {
+        let result = await sequelize.query("DELETE FROM recursos WHERE idRecurso =" + recurso.idRecurso); console.log(result);
         if (result[0].length == 0) {
             return result
         }
